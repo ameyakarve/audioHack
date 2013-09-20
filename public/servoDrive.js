a10c424d0d2893e8e4c599bb6a95a2fadf65cfd2
Ameya Karve <ameya.karve@gmail.com>
2013-09-20 09:30:15 +0530

    push for JSFoo 2.0

--- public/servoDrive.js
+++ public/servoDrive.js
@@ -3,3 +3,7 @@ socket.on('servoDriveSpeedOutput', function (data) {
   LEFT_SPEED = data.LEFT_SPEED;
   RIGHT_SPEED = data.RIGHT_SPEED;
 });
+setInterval(function () {
+  document.getElementById('l'+ LEFT_SPEED).play();
+  document.getElementById('r'+ RIGHT_SPEED).play();
+}, 500);
